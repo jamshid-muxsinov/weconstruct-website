b1dfaa85207cc776c1a136001f7cf3ac9c064acd
jamshid-muxsinov <jmuxsinovjj@gmail.com>
2025-08-14 22:07:22 +0500

    Исправление 7

--- src/forms/quote_forms.py
+++ src/forms/quote_forms.py
@@ -2,6 +2,10 @@ from starlette_wtf import StarletteForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length, Optional as Opt, Regexp
 
+PHONE_REGEX = r'^\+?[0-9\s()-]+$'
+PHONE_ERROR_MESSAGE = "Номер телефона может содержать только цифры, +, -, скобки и пробелы."
+PHONE_TITLE_MESSAGE = "Допустимые символы: 0-9, +, -, (), пробел."
+
 class GeneralQuoteForm(StarletteForm):
     name = StringField(
         'Ваше имя',
@@ -9,17 +13,18 @@ class GeneralQuoteForm(StarletteForm):
         render_kw={"placeholder": " "}
     )
     phone = StringField(
-    'Ваш телефон',
-    validators=[
-        DataRequired(message="Пожалуйста, укажите ваше имя."),
-        Length(min=9, max=13, message="Некорректная длина номера."),
-        Regexp(
-            regex=r'^\+?[0-9\s()-]*$', 
-            message="Номер телефона может содержать только цифры и знак +"
-        )
-    ],
-    render_kw={"placeholder": " ", "type": "tel"}
-)
+        'Ваш телефон',
+        validators=[
+            DataRequired(message="Пожалуйста, укажите ваш телефон."),
+            Regexp(regex=PHONE_REGEX, message=PHONE_ERROR_MESSAGE)
+        ],
+        render_kw={
+            "placeholder": " ", 
+            "type": "tel",
+            "pattern": PHONE_REGEX,    
+            "title": PHONE_TITLE_MESSAGE 
+        }
+    )
     message = TextAreaField(
         'Ваше сообщение (необязательно)',
         validators=[Opt(), Length(max=2000)],
@@ -33,15 +38,16 @@ class QuoteForm(StarletteForm):
         render_kw={"placeholder": " "}
     )
     phone = StringField(
-    'Ваш телефон *',
-    validators=[
-        DataRequired(message="Пожалуйста, укажите ваш телефон."),
-        Length(min=9, max=13, message="Некорректная длина номера."),
-        Regexp(
-            regex=r'^\+?[0-9\s()-]*$', 
-            message="Номер телефона может содержать только цифры и знак +"
-        )
-    ],
-    render_kw={"placeholder": " ", "type": "tel"}
-)
+        'Ваш телефон *',
+        validators=[
+            DataRequired(message="Пожалуйста, укажите ваш телефон."),
+            Regexp(regex=PHONE_REGEX, message=PHONE_ERROR_MESSAGE)
+        ],
+        render_kw={
+            "placeholder": " ", 
+            "type": "tel",
+            "pattern": PHONE_REGEX,     
+            "title": PHONE_TITLE_MESSAGE  
+        }
+    )
     message = TextAreaField('Комментарий', validators=[Opt()])
