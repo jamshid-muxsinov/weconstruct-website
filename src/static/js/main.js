2050a11b5f79ee9a3d5391cac77d5ead66824128
jamshid-muxsinov <jmuxsinovjj@gmail.com>
2025-08-16 10:46:02 +0500

    Исправление 34

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,37 +1,4 @@
-document.addEventListener("alpine:init", () => {
-  Alpine.data("stats", () => ({
-    projects: 0,
-    days: 0,
-    clients: 0,
-    warranty: 0,
-    animateCounter(ref, target, suffix = '') {
-      const el = this.$refs[ref];
-      if (!el) return;
-      
-      let start = 0;
-      const duration = 2000;
-      const frameDuration = 1000 / 60;
-      const totalFrames = Math.round(duration / frameDuration);
-      let frame = 0;
-
-      const counter = setInterval(() => {
-        frame++;
-        const progress = frame / totalFrames;
-        const current = Math.round(target * progress);
-        
-        el.innerText = current + suffix;
-
-        if (frame === totalFrames) {
-          clearInterval(counter);
-          el.innerText = target + suffix; // Убедимся, что в конце точное значение
-        }
-      }, frameDuration);
-    }
-  }));
-});
-
 document.addEventListener("DOMContentLoaded", () => {
-  // Плавный скролл по якорям
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
@@ -47,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  // Автоматическое закрытие модального окна после успеха
   document.body.addEventListener('htmx:afterSwap', function(event) {
     const target = event.detail.target;
     if (target.querySelector('.quote-success')) {
