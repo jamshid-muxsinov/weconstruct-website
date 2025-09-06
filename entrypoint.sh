69115a526797aa4d923195aa04369db52bdb62a8
tasheet <jmuxsinovjj@gmail.com>
2025-09-06 15:05:40 +0500

    06092025 1505

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
+
+# Выходить из скрипта при любой ошибке
 set -e
 
-echo "--> Waiting for database at host 'db' on port 5432..."
-while ! nc -z db 5432; do
+# --- Ожидание готовности базы данных ---
+# Извлекаем хост и порт из DATABASE_URL
+DB_HOST=$(echo $DATABASE_URL | cut -d'@' -f2 | cut -d':' -f1)
+DB_PORT=$(echo $DATABASE_URL | cut -d':' -f3 | cut -d'/' -f1)
+
+echo "--> Waiting for database at host '$DB_HOST' on port $DB_PORT..."
+
+# Используем netcat для проверки, доступен ли порт
+while ! nc -z $DB_HOST $DB_PORT; do
   sleep 1
 done
+
 echo "--> Database is ready!"
 
+# --- Применение миграций Alembic ---
+echo "--> Applying database migrations..."
+alembic upgrade head
+
+echo "--> Migrations applied!"
 echo "--> Starting application..."
+
+# --- Запуск основной команды ---
+# exec "$@" выполняет команду, переданную в entrypoint
+# (в нашем случае, это команда gunicorn из docker-compose.yml)
 exec "$@"
